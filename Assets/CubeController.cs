bcaf46e6c837837f8aa841985744d2873cba3f41
Sumire Kikuta <paddyfield815@gmail.com>
2018-07-03 13:09:21 +0900

    Lesson7 「5.キューブを配置しよう」作業作業途中です。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    // Use this for initialization
+
+    //キューブの移動速度
+    private float speed = -0.2f;
+
+    //消滅速度
+    private float deadLine = -10;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        //画面外に出たら破棄する
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
